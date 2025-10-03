d39c517660cae059d77f6604ac7531a2d3fbe83e
Liam Conrad <liam@w4pho.net>
2025-10-03 14:31:24 -0500

    good for now

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+echo "This will perform automated setup of the uConsole. Your console will be rebooted"
+echo "when complete. This may take some time."
+echo ""
+read -p "Press ENTER to continue, or CTRL-C to quit."
+
+export DIR_UCS="$HOME/.uc_setup"
+export DIR_UCS_GIT="$DIR_UCS/git"
+export DIR_UCS_BAK="$HOME/backups"
+export DIR_UCS_FIRMWARE="$DIR_UCS_BAK/boot/firmware"
+export DIR_UCS_ETC_DEFAULT="$DIR_UCS_BAK/etc/default"
+export DIR_UCS_ETC_CHRONY="$DIR_UCS_BAK/etc/chrony"
+
+export FILE_UCS_FW_CFG_TXT="/boot/firmware/config.txt"
+export FILE_UCS_ETC_GPSD="/etc/default/gpsd"
+export FILE_UCS_ETC_CHRONY="/etc/chrony/chrony.conf"
+
 # Increase the charging rates of the two 18650's (I'm using LiteCore 3500mAh Li-ion cells.)
 echo 'KERNEL=="axp20x-battery", ATTR{constant_charge_current_max}="2200000", ATTR{constant_charge_current}="2000000"' | sudo tee /etc/udev/rules.d/99-uconsole-charging.rules
 
@@ -13,11 +29,30 @@ sudo apt install libgpiod-dev libyaml-cpp-dev libbluetooth-dev libusb-1.0-0-dev 
 # Small setup for PPS
 sudo bash -c "echo 'pps-gpio' >> /etc/modules"
 
+# Backups, and file copies
+mkdir -p $DIR_UCS_GIT
+mkdir -p $DIR_UCS_FIRMWARE
+mkdir -p $DIR_UCS_ETC_DEFAULT
+mkdir -p $DIR_UCS_ETC_CHRONY
+
+git clone https://github.com/storskegg/uconsole.git $DIR_UCS_GIT
+
+[[ -f $FILE_UCS_FW_CFG_TXT ]] && cp $FILE_UCS_FW_CFG_TXT $DIR_UCS_FIRMWARE
+[[ -f $FILE_UCS_ETC_GPSD ]] && cp $FILE_UCS_ETC_GPSD $DIR_UCS_ETC_DEFAULT
+[[ -f $FILE_UCS_ETC_CHRONY ]] & cp $FILE_UCS_ETC_CHRONY $DIR_UCS_ETC_CHRONY
+
+cd $DIR_UCS_GIT/fs
+cp ./boot/firmware/config.txt $FILE_UCS_FW_CFG_TXT
+cp ./etc/default/gpsd $FILE_UCS_ETC_GPSD
+cp ./etc/chrony/chrony.conf $FILE_UCS_ETC_CHRONY
+cp ./home/w4pho/.inputrc $HOME/.inputrc
+
 # ADS-B auto-installers
 sudo bash -c "$(wget -O - https://github.com/wiedehopf/adsb-scripts/raw/master/readsb-install.sh)"
 sudo bash -c "$(wget -nv -O - https://github.com/wiedehopf/tar1090/raw/master/install.sh)"
 
-# Disable Devterm Printer service from Rex's image
-sudo systemctl stop devterm-printer.service
-sudo systemctl disable devterm-printer.service
+# Disable Devterm Printer service from Rex's image; required for meshtasticd
+# sudo systemctl stop devterm-printer.service
+# sudo systemctl disable devterm-printer.service
 
+sudo reboot now
