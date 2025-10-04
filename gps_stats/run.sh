39af7fa9b550579a04fd749f23bf02e4b1d9e600
Liam Conrad <liam@w4pho.net>
2025-10-04 15:19:00 -0500

    fail if not sudo

--- gps_stats/run.sh
+++ gps_stats/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
 PATH_STATS=/var/log/chrony/statistics.log
 
 echo "You'll want to have let the GPS run for at least 1 hour or more. If you haven't"
@@ -9,7 +14,7 @@ read -p "Press Enter to continue, or Ctrl+C to abort."
 
 [[ -f "$PATH_STATS" ]] || { echo "Statistics log not found at $PATH_STATS"; exit 1; }
 
-tail -100 "$PATH_STATS" > ./chrony_statistics.log
+tail -1000 "$PATH_STATS" > ./chrony_statistics.log
 
 NUM_LINES=$(wc -l ./chrony_statistics.log | awk '{print $1}')
 [[ $NUM_LINES -lt 100 ]] && { echo "Not enough statistics in the log. Wait longer."; exit 1; }
